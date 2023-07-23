59f5209c8b6bc5f5c6605831d492a9d0bffb8550
Nahoom <nahoombelete24@gmail.com>
2023-07-23 14:08:52 +0200

    "Add calculations for stock price, ratio, lower/upper bounds and trigger alert. Edit export interface"

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    //Calculate stock prices
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+    //Ratio calculation
+    const ratio = priceABC / priceDEF;
+    //Lower and upper bound calculations
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    return {
+        price_abc: priceABC,
+        price_def: priceDEF,
+        ratio,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? 
+          serverResponds[0].timestamp : serverResponds[1].timestamp, // selects the most recent timestamp
+        upper_bound: upperBound,
+        lower_bound: lowerBound,
+        //If ratio is above or below the bounds return the ratio, if not return undefined
+        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio: undefined,
+    };
   }
 }
